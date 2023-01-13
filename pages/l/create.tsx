c0b9c711f9459fc4ed6ced75994428b557de24e3
Nguyen Quoc Huy <nqh.d3v@gmail.com>
2023-01-14 03:44:04 +0700

    Update

--- pages/l/create.tsx
+++ pages/l/create.tsx
@@ -8,8 +8,8 @@ const CreateShortenLink = () => {
       title="Create Shorten Link"
       description="This page allow you to create a new shorten link from your long link"
     >
-      <div className="max-w-[550px] pt-5 sm:pt-[100px] m-auto">
-        <h2 className="text-2xl sm:text-3xl code font-bold mb-5">
+      <div className="max-w-[550px] pt-5 m-auto">
+        <h2 className="text-2xl sm:text-3xl code font-bold mb-1">
           <span className="var">url</span>
           {" = "}
           <span className="operator">
@@ -18,6 +18,14 @@ const CreateShortenLink = () => {
           <span className="class">ShortenURL</span>
           {"();"}
         </h2>
+        <div className="code comment mb-5">
+          document about this app?
+          {' '}
+          <a href="https://nqhuy.dev/coding/shorten-url-app" rel="noreferrer" aria-label="This link will redirect you to app's document">
+            here
+          </a>
+          {'.'}
+        </div>
         <ShortenURLCreate />
         <ShortenURLRemove />
       </div>
