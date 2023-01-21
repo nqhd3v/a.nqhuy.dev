d3e00b8d135aaa48426b390f560d0b42555bc31d
Nguyen Quoc Huy <nqh.d3v@gmail.com>
2023-01-21 11:34:40 +0700

    Add authenticated information for default redirect when logged in

--- pages/auth/info.tsx
+++ pages/auth/info.tsx
@@ -4,6 +4,16 @@ import { useEffect } from "react";
 import { useFirebaseAuth, withFirebaseAuth } from "../../components/Firebase/FirebaseAuthWrapper";
 import LayoutAnimated from "../../components/wrapper/LayoutAnimated";
 
+const AuthValue = ({ name, value }: { name: string, value: string | undefined | null }) => {
+  return (
+    <div className="code">
+      <span className="var">{name}</span>
+      {" = "}
+      {value ? <span className="str">{value}</span> : <span className="def">undefined</span> }
+    </div>
+  )
+}
+
 const AuthInfo = () => {
   const router = useRouter();
   const { user, loading } = useFirebaseAuth();
@@ -14,7 +24,7 @@ const AuthInfo = () => {
     }
   }, [user, loading]);
 
-  if (!user || loading) {
+  if (loading) {
     return (
       <LayoutAnimated title="Auth Info" description="This page will display your authenticated information">
         <div className="w-full max-w-[500px] m-auto min-h-screen py-10">
@@ -26,7 +36,18 @@ const AuthInfo = () => {
     )
   }
 
-  console.log(user);
+  if (!user) {
+    return (
+      <LayoutAnimated title="Auth Info" description="This page will display your authenticated information">
+        <div className="w-full max-w-[500px] m-auto min-h-screen py-10">
+          <div className="p-5 border border-gray-400 dark:border-gray-600 rounded-md">
+            You need to login to view your authenticated information!
+          </div>
+        </div>
+      </LayoutAnimated>
+    )
+  }
+
   return (
     <LayoutAnimated title="Auth Info" description="This page will display your authenticated information">
       <div className="w-full max-w-[500px] m-auto min-h-screen py-10">
@@ -35,23 +56,10 @@ const AuthInfo = () => {
             {'user info '}
             {user.metadata.lastSignInTime ? ` - logged in at "${moment(user.metadata.lastSignInTime).format('DD/MM/YYYY HH:mm:ss')}"` : ''}
           </div>
-          <div className="code">
-            <span className="var">displayName</span>
-            {" = "}
-            {user.displayName ? <span className="str">{user.displayName}</span> : <span className="def">undefined</span> }
-          </div>
-          <div className="code">
-            <span className="var">email</span>
-            {" = "}
-            {user.email ? <span className="str">{user.email}</span> : <span className="def">undefined</span> }
-          </div>
+          <AuthValue name="displayName" value={user.displayName} />
+          <AuthValue name="email" value={user.email} />
           <div className="code comment">auth info</div>
-          <div className="code">
-            <span className="var">provider</span>
-            {" = "}
-            {user.providerData.length > 0 ? <span className="str">{user.providerData[0].providerId}</span> : <span className="def">undefined</span> }
-            {";"}
-          </div>
+          <AuthValue name="provider" value={user.providerData[0]?.providerId} />
         </div>
       </div>
     </LayoutAnimated>
